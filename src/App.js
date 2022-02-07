e2082a5e8e1cf57e448c3de17d00737815a6f590
Dhiraj <dhirajgede@gmail.com>
2022-02-07 21:03:35 +0530

    Get and post data on Firebase realtime DB

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import database from './firebase';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-        >
-          Deployed reactjs
-        </a>
-      </header>
-    </div>
-  );
+const [name , setName] = useState();
+const [age , setAge] = useState();
+var[valuex, setVal]= useState();
+var [val,getVal]=useState();
+
+	
+// Push Function
+const Push = () => {
+	database.ref("user").set({
+	name : name,
+	age : age,
+	}).catch(alert);
+}
+const Puss = () => {
+	database.ref("ultra_sonic").set({
+	valuex: valuex,
+
+	}).catch(alert);
+}
+const Pus = () => {
+	database.ref("ultra_sonic").on({
+	val: val,
+
+	}).catch(alert);
+}
+
+
+const dbRef = database.ref('ultra_sonic');
+dbRef.child("value").child(getVal).get().then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+dbRef.on('value', snapshot => {
+  console.log(snapshot.val().valuex);
+  val=snapshot.val().valuex;
+});
+
+
+console.log(`value =${val}`)
+
+
+return (
+	<div className="App" style={{marginTop : 250}}>
+	<center>
+  
+	<input placeholder="Enter your name" value={name}
+	onChange={(e) => setName(e.target.value) } />
+ 
+	<br/><br/>
+	<input placeholder="Enter your age"  value={age}
+	onChange={(e) => setAge(e.target.value)}/>
+	<br/><br/>
+	<input placeholder="Enter your valuex"  value={valuex}
+	onChange={(e) => setVal(e.target.value)}/>
+	<br/><br/>
+  <h1 >value = {val}</h1>
+	<br/><br/>
+	<button onClick={Push}>PUSH</button>
+	<button onClick={Puss}>PUSS</button>
+	{/* <button onClick={Puss}>PUS</button> */}
+	</center>
+  
+	</div>
+);
 }
 
 export default App;
---
+++ src/temp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Temp = () => {
+    return (
+        <div>
+            Dhiraj
+        </div>
+    );
+}
+
+export default Temp;
+
