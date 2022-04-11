31f63a4c3889f0745e34698123a77f0f3d163acd
TusharKarade <tusharkarade10@gmail.com>
2022-04-11 13:02:12 +0530

    email automation

--- src/components/Values/nodeMailer.js
+++ src/components/Values/nodeMailer.js
@@ -1,9 +1,23 @@
-import firebase from '../../util/firebase.js';
+// import firebase from '../../util/firebase.js';
+const firebase = require('firebase');
 
-import nodemailer from 'nodemailer';
+const nodemailer = require('nodemailer');
 
-import cron from 'node-cron';
+const cron = require('node-cron');
 
+const firebaseConfig = {
+  // Your Credentials
+  apiKey: "AIzaSyB-JVOhbRXzkk3XkMLeHpXSwVirK6tJSsU",
+  authDomain: "aquaponics-5a5a7.firebaseapp.com",
+  databaseURL: "https://aquaponics-5a5a7-default-rtdb.firebaseio.com",
+  projectId: "aquaponics-5a5a7",
+  storageBucket: "aquaponics-5a5a7.appspot.com",
+  messagingSenderId: "275682898912",
+  appId: "1:275682898912:web:31073ba85ccff15ef8f185",
+  measurementId: "G-JDW35B8V2K",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database().ref();
 
